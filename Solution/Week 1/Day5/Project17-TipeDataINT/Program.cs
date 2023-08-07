42f9c361d47e023fca90150c644d9966b3aa5f31
LAPTOP-53DV9PCC\Kirana <kirana0701@gmail.com>
2023-08-07 10:02:28 +0700

    update Day 5

---
+++ Solution/Week 1/Day5/Project17-TipeDataINT/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program 
+{
+	static void Main()
+	{
+		int a = 1;
+		int b = 2;
+		Console.WriteLine((a+b) == 3); // True 
+		
+		float c = 0.1f;
+		float d = 0.2f;
+		Console.WriteLine((c+d) == 0.3f); // True
+		Console.WriteLine((c+d));
+		
+		double e = 0.1d;
+		double f = 0.2d; 
+		Console.WriteLine((e+f) == 0.3d); // False
+		Console.WriteLine((e+f));
+		
+		decimal g = 0.1M;
+		decimal h = 0.2M;
+		Console.WriteLine((g+h) == 0.3M); // True 
+		Console.WriteLine((g+h));
+}
+}
