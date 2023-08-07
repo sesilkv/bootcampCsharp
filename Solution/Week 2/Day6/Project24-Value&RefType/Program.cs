a6190830bc9904986a9ebdd6928c56fb7b6b729e
LAPTOP-53DV9PCC\Kirana <kirana0701@gmail.com>
2023-08-08 00:05:49 +0700

    update project 24 25

--- Solution/Week 2/Day6/Project24-Value&RefType/Program.cs
+++ Solution/Week 2/Day6/Project24-Value&RefType/Program.cs
@@ -4,13 +4,27 @@
 	{
 		int a = 5;
 		Console.WriteLine(a);
-		int b = ;
+		int b = a;
+		b = b + 2;
 		Console.WriteLine(b);
+		
+		Value value = new();
+		value.a = 5;
+		Console.WriteLine(value.a);
+		Value value2 = new();
+		// int value2 = value.a;
+		// Console.WriteLine(value2);
+		
+		string x = "5";
+		string y = x;
+		y = y + 2;
+		Console.WriteLine(x);
+		Console.WriteLine(y);
 	}
 }
 
 public class Value
 {
 	public int a;
-	public int b;
+	// public int b;
 }
--- Solution/Week 2/Day6/Project25-Enum/Program.cs
+++ Solution/Week 2/Day6/Project25-Enum/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		// foreach (var day in Enum.GetValues(typeof(DaysOfWeek)))
+		// {
+		// 	Console.WriteLine(day);
+		// }
+		
+		
+		DaysOfWeek today = DaysOfWeek.Monday;
+		Console.WriteLine((int)today);
+		Console.WriteLine("Today is: " + today);
+		switch (today)
+		{
+			case DaysOfWeek.Sunday:
+				Console.WriteLine("Sunday");
+				break;
+			case DaysOfWeek.Monday:
+				Console.WriteLine(DaysOfWeek.Monday);
+				break;
+			case DaysOfWeek.Tuesday:
+				Console.WriteLine("Tuesday");
+				break;
+		}
+		
+		
+		DaysOfWeek day1 = DaysOfWeek.Monday;
+		Console.WriteLine(day1); // output:
+		Console.WriteLine((int)day1);  // output:
+
+		string userDays = "Monday";
+		DaysOfWeek day = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), userDays);
+		Console.WriteLine("Parsed enum value : " + day);
+		Console.WriteLine("Parsed enum value : " + (int)day);
+
+		Console.WriteLine((int)DaysOfWeek.Monday);
+		DaysOfWeek y = (DaysOfWeek)0;
+	}
+}
+
+public enum DaysOfWeek
+	{
+		Monday=0,
+		Tuesday,
+		Wednesday,
+		Thursday,
+		Friday,
+		Saturday,
+		Sunday
+	}
