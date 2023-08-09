66f65e55d1e009f5dbc8c711712debc917f2d611
LAPTOP-53DV9PCC\Kirana <kirana0701@gmail.com>
2023-08-10 00:38:18 +0700

    update Day 8

--- Solution/Week 2/Day8/Project33-TryCatch/Program.cs
+++ Solution/Week 2/Day8/Project33-TryCatch/Program.cs
@@ -1,18 +1,27 @@
-﻿class Program {
+﻿class Program
+{
 	static void Main()
 	{
-		Console.WriteLine("Hello");
+		// Console.WriteLine("Hello");
 		try
 		{
+			Array();
 			Divide(10, 0);
-		} catch (Exception e)
+		}
+		catch (Exception e)
 		{
 			Console.WriteLine(e.Message);
 		}
 	}
-	
+
 	static void Divide(int x, int y)
 	{
-		int z = x/y;
+		int z = x / y;
+	}
+
+	static void Array()
+	{
+		int[] num = { 1, 2, 3, 4 };
+		Console.WriteLine(num[2]);
 	}
 }
---
+++ Solution/Week 2/Day8/Project34-Parse&TryParse/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program 
+{
+	static void Main(string[] args)
+	{
+		Parse();
+		TryParse();
+		// try
+		// {
+		// 	TryParse();
+		// } catch (System.FormatException e) 
+		// {
+		// 	Console.WriteLine(e.Message);
+		// }
+	}
+	
+	//konversi string ke tipe data int
+	//PARSE digunakan saat kita yakin akan string yg bersangkutan
+	static void Parse()
+	{
+		string input = "22";
+		int x = int.Parse(input);
+		Console.WriteLine(x);
+	}
+	
+	//TryParse digunakan saat kita tidak yakin, dan ingin mengecek
+	static void TryParse()
+	{
+		string input = "H";
+		bool status = int.TryParse(input, out int x);
+		Console.WriteLine(x);
+	}
+}
