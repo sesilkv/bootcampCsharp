da50e1b0af8ad0981341cc4950a94772bf558a6c
LAPTOP-53DV9PCC\Kirana <kirana0701@gmail.com>
2023-09-01 01:16:42 +0700

    update Day23

---
+++ Solution/Week 5/Day23/Project76-DocumentationXML/MyLib.cs
@@ -0,0 +1,38 @@
+namespace DocumentationXML
+{
+	class MyLib
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static double Add(double a, double b)
+		{
+			return a + b;
+		}
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <param name=""></param>
+		/// <returns></returns>
+		public static double Multiply(double a, double b)
+		{
+			return a*b;
+		}
+		
+		public static double Divide(double a, double b)
+		{
+			return a/b;
+		}
+		
+		public static double Subtract(double a, double b)
+		{
+			return a - b;
+		}
+	}
+}
--- Solution/Week 5/Day23/Project76-DocumentationXML/Program.cs
+++ Solution/Week 5/Day23/Project76-DocumentationXML/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DocumentationXML;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		// MyLib myLib = new MyLib();
+		
+		double a = 4.0;
+		double b = 2.5;
+		
+		// static can't use instance
+		double Add = MyLib.Add(a, b);
+		double Multiply = MyLib.Multiply(a, b);
+		double Divide = MyLib.Divide(a, b);
+		double Subtract = MyLib.Subtract(a, b);
+		string res = $"{Add} {Multiply} {Divide} {Subtract}";
+		Console.WriteLine($"{a} + {b} = {res}");
+	}
+}
